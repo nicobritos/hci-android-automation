1b051cc998ac7b4e6025232bf2ac0bb973df5ccd
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2019-11-23 20:14:26 -0300

    new endpoint works

---
+++ app/src/main/java/com/hci/StarkIndustries/ui/APIReloadingFragment.java
@@ -0,0 +1,38 @@
+package com.hci.StarkIndustries.ui;
+
+import android.util.Log;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.hci.StarkIndustries.MainActivity;
+import com.hci.StarkIndustries.R;
+import com.hci.StarkIndustries.data.remote.Api;
+
+public abstract class APIReloadingFragment extends Fragment implements ChangeEndpointFragment.OnInputSelected {
+
+    @Override
+    public void sendInput(String newAdress) {
+        Api.setEndpoint(newAdress);
+        final MainActivity act = (MainActivity) getActivity();
+        final String oldAddress = act.sharedPreferences.getString("API ENDPOINT",getString(R.string.defaultAPIEndpoint));
+        act.sharedPreferences.edit().putString("API ENDPOINT",newAdress).commit();
+
+
+        Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.coordinatorLayour)
+                ,R.string.newEndpointText,Snackbar.LENGTH_LONG);
+        snackbar.setAction(R.string.newEndpointUndo,l->{
+
+            Api.setEndpoint(oldAddress);
+            act.sharedPreferences.edit().putString("API ENDPOINT",oldAddress).commit();
+            reloadPage();
+
+        });
+
+        snackbar.show();
+
+        reloadPage();
+    }
+
+    abstract protected void reloadPage();
+}
